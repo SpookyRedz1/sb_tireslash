8f1ff08db034cce4f3fb12f2b9ab4f5a02e8a787
SpookyRedz1 <77546581+SpookyRedz1@users.noreply.github.com>
2022-03-20 12:00:05 -0500

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,8 @@
+--[[
+   Scripted By: SpookyRedz#2768 (𝗦𝗕 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗺𝗲𝗻𝘁™)
+--]]
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+server_script "server.lua"
+client_script "client.lua"
